f4ed4036dc1aa41ee801198907cfc33cfdb8d323
yabeshgotame <gotameyabesh@gmail.com>
2025-03-23 23:09:10 +0545

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,10 +2,12 @@
 const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('nav-links');
 
-hamburger.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-  hamburger.classList.toggle('active');
-});
+if (hamburger && navLinks) {
+  hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    hamburger.classList.toggle('active');
+  });
+}
 
 // Search Bar Toggle (for Blogs Page)
 const searchIcon = document.getElementById('search-icon');
