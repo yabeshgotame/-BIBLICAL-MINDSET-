b9000c6bdeb820c03eee5ee5e9cc3db04ed54bac
yabeshgotame <gotameyabesh@gmail.com>
2025-03-23 10:41:26 +0545

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Hamburger Menu Toggle
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+  hamburger.classList.toggle('active');
+});
